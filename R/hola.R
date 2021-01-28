9d94c9b4b845e94433f349d3e232c504fe872676
Leonardo Collado Torres <lcolladotor@gmail.com>
2021-01-28 13:48:56 -0500

    Agregamos la función hola()

---
+++ R/hola.R
@@ -0,0 +1,15 @@
+#' Saludos
+#'
+#' Esta función te saluda desde ConectaR2021
+#'
+#' @param x Un `character(1)` con tu nombre.
+#'
+#' @return Imprime un mensaje con el saludo.
+#' @export
+#'
+#' @examples
+#'
+#' hola("Olga")
+hola <- function(x) {
+    message("Hola ", x, ", ¡te mandamos saludos desde ConectaR2021!")
+}
